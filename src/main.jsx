441ffe6387ecd0f8745a0a89379db36b2a732b12
Mia Rose Elbo Armstrong <miaroseelbo@gmail.com>
2025-12-09 21:12:43 +0100

    test3

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./styles.css";
 
 console.log("🔥 React main.jsx loaded");
-
-import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <HashRouter>
